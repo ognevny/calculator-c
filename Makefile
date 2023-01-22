695321456f48ddccaa55cb9dab3a212ff9832362
KieranCrossland <kierancrossland1st@gmail.com>
2023-01-22 01:00:05 +0000

    started rewrite

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 default:
 	cc calculator.c -o bin/calculator
+
+newcalc:
+	cc newcalc.c -o bin/newcalc
+	bin/newcalc
---
+++ newcalc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	printf("Enter query Eg: '5 - 4'.\n");
+	scanf 
+	
+
+	char operator[1];
+	int number1;
+	int number2;
+	
+}
+
